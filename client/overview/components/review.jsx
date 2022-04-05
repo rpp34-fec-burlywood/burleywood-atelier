84e5abb722e0868ed84e5f00bbf7e389b143b57b
Isaiah Smith <isaiah.cornelius.smith@gmail.com>
2022-04-04 19:10:27 -0700

    Set up reviews skeleton

---
+++ client/overview/components/review.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Review extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return(
+      <div>Review</div>
+    );
+  }
+
+}
+
+export default Review;
---
+++ client/overview/components/reviewsWidget.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class ReviewsWidget extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return(
+      <div>Reviews Widget</div>
+    );
+  }
+
+}
+
+export default ReviewsWidget;
