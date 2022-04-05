a37499976d94a70d303b71eb0ee6d91a5ba06ee7
Zhaowei <ding.zhaowei.ding@gmail.com>
2022-04-04 21:13:58 -0400

    Changed PORT to use process.env.PORT or 3000;

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 
 require('dotenv').config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const API_KEY = process.env.API_KEY;
 
 
