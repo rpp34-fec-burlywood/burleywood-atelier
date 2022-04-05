1093678258b7086f493741f849eb6d7a864344fa
Isaiah Smith <isaiah.cornelius.smith@gmail.com>
2022-04-04 19:10:27 -0700

    Set up reviews skeleton

---
+++ client/overview/components/ratingsBreakdown.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class RatingsBreakdown extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>RatingsBreakdown</div>
+    );
+  }
+}
+
+export default RatingsBreakdown;
---
+++ client/overview/components/reviewList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class ReviewList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return(
+      <div>Review List</div>
+    );
+  }
+
+}
+
+export default ReviewList;
