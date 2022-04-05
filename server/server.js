57a15b875080e08de9977824d0441363411e7171
Zhaowei <ding.zhaowei.ding@gmail.com>
2022-04-04 21:01:24 -0400

    -Set up .env in server using dotenv package -setup basic request using axios to api

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const axios = require('axios');
+
 require('dotenv').config();
-
-
-const app = express();
 const PORT = process.env.PORT;
 const API_KEY = process.env.API_KEY;
 
+
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -14,6 +15,24 @@ app.use(express.static(__dirname + '/../public'));
 
 app.get('/ping', (req, res) => {
   res.send('pong')
+});
+
+app.get('/api/products', (req, res) => {
+  axios({
+    method: 'GET',
+    url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products',
+    headers: { Authorization: API_KEY }
+  })
+    .then((response)=>{
+      console.log('-- Get Products OK\n', response.data)
+      res.status(200);
+      res.send(response.data);
+    })
+    .catch((err)=>{
+      console.log('-- Get Products FAILED\n', err);
+      res.status(500);
+      res.send(err);
+    });
 });
 
 app.listen(PORT, () => {
