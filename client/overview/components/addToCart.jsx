1196ecbb4e29cddb8589087d4a7908ed236559bf
Zhaowei <ding.zhaowei.ding@gmail.com>
2022-04-16 02:41:38 -0400

    Added size and quanity selector. both elements work together

--- client/overview/components/addToCart.jsx
+++ client/overview/components/addToCart.jsx
@@ -1,19 +1,69 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import SizeSelect from './cartComponents/sizeSelect.jsx';
+import QuantitySelect from './cartComponents/quantitySelect.jsx'
+//AddToCart
 
 class AddToCart extends React.Component {
   constructor(props) {
     super(props);
 
+    //selected current state
+    this.state = {
+      currSize: null,
+      quantity: 0,
+      currSizeStock: null
+    }
 
+    this.selectSizeHandler = this.selectSizeHandler.bind(this);
+    this.selectQuanityHandler = this.selectQuanityHandler.bind(this);
+    this.stockHandler = this.stockHandler.bind(this);
+  }
+
+  // NEED Way to handle Edge case of Null SKU, (infinity Stone)
+
+  selectSizeHandler(e) {
+    e.preventDefault();
+    this.setState({
+      currSize: e.target.value
+    });
+  }
+
+  selectQuanityHandler(e) {
+    e.preventDefault();
+    this.setState({
+      quantity: e.target.value
+    });
+  }
+
+  stockHandler(e) {
+    e.preventDefault();
+    var index = e.target.selectedIndex;
+    var stock = e.target[index].attributes.stock.value;
+    this.setState({
+      currSizeStock: Number(stock)
+    });
   }
 
 
   render() {
-    return(
-      <div>AddToCart</div>
-    );
+    if (this.props.selectedStyle) {
+      return (
+        <div id="addToCart">
+          <SizeSelect
+            skus={this.props.selectedStyle.skus}
+            selectSizeHandler={this.selectSizeHandler}
+            stockHandler = {this.stockHandler} />
+          <QuantitySelect
+            skus={this.props.selectedStyle.skus}
+            currSize={this.state.currSize}
+            currSizeStock={this.state.currSizeStock}
+            selectQuanityHandler={this.selectQuanityHandler} />
+        </div>
+      );
+    }
+    return <div id="addToCart"></div>
   }
-
 }
 
 export default AddToCart;
