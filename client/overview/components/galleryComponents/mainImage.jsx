4eb2c36718973407510788180a0a014e74f34316
Zhaowei <ding.zhaowei.ding@gmail.com>
2022-04-23 17:41:07 -0400

    Overvew Image expansion functionally Added! with Scrolling! -enlarged image should over centire overview widget. -image is scrollable, auto detected -left/right arrow renders conditionally if more than 1 image
    -still need left and right click.... this may be to be bunddled with a handler all the way up to were the main image index is kept - (app.state level)

---
+++ client/overview/components/galleryComponents/mainImage.jsx
@@ -0,0 +1,69 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import $ from 'jquery';
+
+class MainImage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      expanded: false,
+      height: 500
+    }
+
+    this.clickHandler = this.clickHandler.bind(this);
+    this.renderArrows = this.renderArrows.bind(this)
+  }
+
+  // mainImage will need to be its own component for expanded view
+
+  clickHandler() {
+    var height = $('#overview').height();
+    this.setState({
+      expanded: !this.state.expanded,
+      height
+    })
+
+  }
+
+  changeStyle(expandedState) {
+    if (!expandedState) {
+      return {};
+    }
+    return {
+      width: '1000px',
+      height: `${this.state.height}px`,
+    }
+  }
+
+  renderArrows(expandedState) {
+    if (expandedState && this.props.selectedStyle.photos.length > 1) {
+      return (
+        <>
+          <button id="mainLeft" className='arrowX'> Left </button>
+          <button id="mainRight" className='arrowX'> Right </button>
+        </>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div className={`mainImageContainer${this.state.expanded ? ' expanded' : ''}`}
+        style={this.changeStyle(this.state.expanded)}>
+        {this.renderArrows(this.state.expanded)}
+        <div className={`mainImageScroll ${this.state.expanded ? ' expanded' : ''}`}>
+          <img className={`mainImage${this.state.expanded ? ' expanded' : ''}`}
+            onClick={this.clickHandler}
+            src={
+              this.props.selectedStyle.photos[this.props.mainImageIndex] ?
+                this.props.selectedStyle.photos[this.props.mainImageIndex].url :
+                this.props.selectedStyle.photos[0].url
+            } />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MainImage;
