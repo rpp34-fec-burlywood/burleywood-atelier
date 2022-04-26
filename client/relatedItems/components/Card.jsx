0378af4e2fb1e8fdc5b4fc62d697d0929fec20d6
nathan qiu <kechengqiu@gmail.com>
2022-04-26 14:24:22 -0500

    fixed css to match overall theme

--- client/relatedItems/components/Card.jsx
+++ client/relatedItems/components/Card.jsx
@@ -9,12 +9,12 @@ import CompareButton from './comapreModal/compareButton.jsx';
 const Card = (props)=> {
   const {product} = props;
   const{original_price, sale_price, photos}  = product.styles
-  const img = photos[0].thumbnail_url || `https://source.unsplash.com/random/300×194/?${product.category}`
+  const img = photos[0].thumbnail_url || `https://source.unsplash.com/random/228×194/?${product.category}`
   if (props.type === 'outfit') {
     return (
       <div className = "card">
         <DeleteButton id={product.id} handleRemove = {props.handleRemoveOutfit}/>
-        <img className ="img" src ={img}  width='230' height='194'/>
+        <img className ="img" src ={img}  width='228' height='194'/>
         <p className='card-category'>{product.category}</p>
         <p className="card-price">{product.name}</p>
         <Price sale ={sale_price} original = {original_price}/>
@@ -26,7 +26,7 @@ const Card = (props)=> {
         //todo CSS
       <div className = "card">
         <CompareButton leftName={props.currProd.name} currProd={props.currProd.features} selectedProd={props.product.features} rightName={props.product.name}/>
-        <img className ="img" src ={img}  width='230' height='194'/>
+        <img className ="img" src ={img}  width='228' height='194'/>
         <p className='card-category'>{product.category}</p>
         <p className="card-price">{product.name}</p>
         <Price sale ={sale_price} original = {original_price}/>
