9d24040f0065c1d2f218b0bf8df6fb97a843c787
Zhaowei <ding.zhaowei.ding@gmail.com>
2022-04-26 00:36:02 -0400

    Modified some tests and Package.json to have more specific tests

--- client/tests/overview.test.js
+++ client/tests/overview.test.js
@@ -1,3 +1,5 @@
+/* eslint-disable testing-library/no-node-access */
+/* eslint-disable testing-library/no-container */
 const React = require('react');
 import { render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom';
@@ -76,19 +78,93 @@ var state = {
       }
     ],
     "skus": "{2313358: {…}, 2313359: {…}, 2313360: {…}, 2313361:…}"
-  }
+  },
+  "currProdStyles": [
+    {
+      "style_id": 398234,
+      "name": "White",
+      "original_price": "450.00",
+      "sale_price": null,
+      "default?": true,
+      "photos": [
+        {
+          "thumbnail_url": "https://images.unsplash.com/photo-1505248254168-1de4e1abfa78?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+          "url": "https://images.unsplash.com/photo-1505248254168-1de4e1abfa78?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1649&q=80"
+        }
+      ]
+    }
+  ],
+  "defaultStyle": {
+    "style_id": 398234,
+    "name": "White",
+    "original_price": "450.00",
+    "sale_price": null,
+    "default?": true,
+    "photos": [
+      {
+        "thumbnail_url": "https://images.unsplash.com/photo-1505248254168-1de4e1abfa78?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+        "url": "https://images.unsplash.com/photo-1505248254168-1de4e1abfa78?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1649&q=80"
+      },
+      {
+        "thumbnail_url": "https://images.unsplash.com/photo-1507920676663-3b72429774ff?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
+        "url": "https://images.unsplash.com/photo-1507920676663-3b72429774ff?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80"
+      },
+      {
+        "thumbnail_url": "https://images.unsplash.com/photo-1544376664-80b17f09d399?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+        "url": "https://images.unsplash.com/photo-1544376664-80b17f09d399?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1525&q=80"
+      },
+      {
+        "thumbnail_url": "https://images.unsplash.com/photo-1513531926349-466f15ec8cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+        "url": "https://images.unsplash.com/photo-1513531926349-466f15ec8cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+      },
+      {
+        "thumbnail_url": "https://images.unsplash.com/photo-1517278322228-3fe7a86cf6f0?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
+        "url": "https://images.unsplash.com/photo-1517278322228-3fe7a86cf6f0?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80"
+      },
+      {
+        "thumbnail_url": "https://images.unsplash.com/photo-1517720359744-6d12f8a09b10?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+        "url": "https://images.unsplash.com/photo-1517720359744-6d12f8a09b10?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80"
+      },
+      {
+        "thumbnail_url": "https://images.unsplash.com/photo-1530821875964-91927b611bad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+        "url": "https://images.unsplash.com/photo-1530821875964-91927b611bad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+      },
+      {
+        "thumbnail_url": "https://images.unsplash.com/photo-1519862170344-6cd5e49cb996?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+        "url": "https://images.unsplash.com/photo-1519862170344-6cd5e49cb996?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+      }
+    ],
+    "skus": "{2313358: {…}, 2313359: {…}, 2313360: {…}, 2313361:…}"
+  },
+  "mainImageIndex": 0
 }
 
 describe("Renders Overview Component", function () {
   test("should render with Product Information", function () {
     render(
       <Overview
-        currProd={state.currProd}
-        currProdStyles={state.currProdStyles}
-        selectedStyle={state.selectedStyle} />
+      currProd={state.currProd}
+      currProdStyles={state.currProdStyles}
+      defaultStyle={state.defaultStyle}
+      selectedStyle={state.selectedStyle}
+      mainImageIndex={state.mainImageIndex} />
     );
     expect(screen.getByText(state.currProd.name)).toBeInTheDocument();
     expect(screen.getByText('$' + state.currProd.default_price)).toBeInTheDocument();
-    expect(screen.getByText(state.currProd.category)).toBeInTheDocument();
+    expect(screen.getByText(state.currProd.name)).toBeInTheDocument();
   });
+})
+
+  describe("Renders Carousel Component", function () {
+    test("should render with  Information", function () {
+      const {container} = render(
+        <Overview
+        currProd={state.currProd}
+        currProdStyles={state.currProdStyles}
+        defaultStyle={state.defaultStyle}
+        selectedStyle={state.selectedStyle}
+        mainImageIndex={state.mainImageIndex} />
+      );
+      expect(container.getElementsByClassName('carouselItem').length).toBeGreaterThan(0);
+    });
 });
