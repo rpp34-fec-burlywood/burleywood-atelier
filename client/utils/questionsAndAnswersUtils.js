7779138755671fee91e52393f3bc1305dd36823f
MiTo0o <76019114+MiTo0o@users.noreply.github.com>
2022-04-27 01:19:30 -0700

    answer sorted on helpfulness

--- client/utils/questionsAndAnswersUtils.js
+++ client/utils/questionsAndAnswersUtils.js
@@ -4,8 +4,22 @@ async function getQuestionsArray (productID) {
 
   let questions = await API.getQuestions(productID);
   let sortedQuestionsArray = questions.results;
+
+  // sort questions array
   sortedQuestionsArray.sort((a, b) => (b.question_helpfulness - a.question_helpfulness))
   questions.results = sortedQuestionsArray
+
+
+  questions.results.forEach(questionObj => {
+
+    let temp = Object.keys(questionObj.answers)
+      .map((answerID) => (questionObj.answers[answerID]));
+    temp.sort((a, b) => (b.helpfulness - a.helpfulness))
+    questionObj.answers = temp
+  });
+
+  console.log(questions.results)
+
   this.setState({questionsList : questions});
 }
 
