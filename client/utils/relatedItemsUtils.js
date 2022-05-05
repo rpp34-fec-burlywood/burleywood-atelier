c9d52715c3e29391dc1fbe2f5dafdb1f202fd26a
Zhaowei <ding.zhaowei.ding@gmail.com>
2022-05-04 22:20:19 -0400

    Matched related products for adding products via unshift

--- client/utils/relatedItemsUtils.js
+++ client/utils/relatedItemsUtils.js
@@ -54,7 +54,7 @@ function handleAddProduct() {
       }
     })
     if(!duplicate) {
-      yourOutfits.push(currentOutfit);
+      yourOutfits.unshiftß(currentOutfit);
       this.setState({outfits: yourOutfits})
       // this.props.outfitUpdater(yourOutfits)
       sessionStorage.setItem('outfits', JSON.stringify(yourOutfits));
