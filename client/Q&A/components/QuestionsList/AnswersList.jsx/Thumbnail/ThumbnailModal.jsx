472d14848400ae46b6fcf20cf696c8e0fc9aa4dc
MiTo0o <76019114+MiTo0o@users.noreply.github.com>
2022-05-06 05:09:44 -0700

    event listener for thumbnail image modal

--- client/Q&A/components/QuestionsList/AnswersList.jsx/Thumbnail/ThumbnailModal.jsx
+++ client/Q&A/components/QuestionsList/AnswersList.jsx/Thumbnail/ThumbnailModal.jsx
@@ -6,12 +6,23 @@ import './ThumbnailModal.css';
 class ThumbnailModal extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = {
-
-    // }
+    this.closeModalEventHandler = this.closeModalEventHandler.bind(this);
   }
 
+  closeModalEventHandler(event) {
+    let modalWindow = document.getElementById("thumbnailModalImage");
+    if (event.target !== modalWindow && !event.target.matches('.qa-thumbnail-icon')) {
+      this.props.closeThumbnailModal();
+    }
+  }
 
+  componentDidMount() {
+    window.addEventListener('click', this.closeModalEventHandler)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('click', this.closeModalEventHandler)
+  }
 
   render() {
     return(
